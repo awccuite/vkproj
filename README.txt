ed263752bee7b5f0ded8f072679b95cd9e5bfce4
aidancuite <awc82@cornell.edu>
2025-04-28 00:27:26 -0400

    changes

---
+++ README.txt
@@ -0,0 +1,8 @@
+This project is built via mingw64. 
+Tested on
+    - SDL3 (3.2)
+    - Vulkan (Using 1.4.309.0)
+    - vk-bootstrap (1.4.33)
+    - gcc (14.2)
+    - pkgconf
+    - cmake (4.0.1)
--- build.sh
+++ build.sh
@@ -1,5 +1,19 @@
+#!/bin/bash
+# Make sure we're executing in an MSYS2 environment
+echo "Starting build with MinGW GCC..."
+
+# Cleanup and rebuild
 rm -rf build
-mkdir build
+mkdir -p build
 cd build
-cmake ..
-make
+
+# Configure with the toolchain file
+echo "Running CMake configuration..."
+cmake -DCMAKE_BUILD_TYPE=Debug \
+      -DCMAKE_TOOLCHAIN_FILE=../mingw-clang-toolchain.cmake \
+      -DCMAKE_VERBOSE_MAKEFILE=ON \
+      ..
+
+# Build with verbose output so we can see what's happening
+echo "Building project..."
+make VERBOSE=1
